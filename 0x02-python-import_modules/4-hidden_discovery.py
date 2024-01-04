fa1b8f50229d83fb63f9f603d76ce8612f8a1143
Mwan3kii <agathawanja303@gmail.com>
2024-01-04 19:31:16 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    f = dir(hidden_4)
+    f = dir()
     for i in range(0, len(f)):
         if f[i][:2] != "__":
         print("{:s}".format(f[i]))
