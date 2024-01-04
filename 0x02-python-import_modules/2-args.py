71feb10e728b04f4f2dd665190f701917b290f17
Mwan3kii <agathawanja303@gmail.com>
2024-01-04 18:43:46 +0300

    lists arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-len(argv)
-if 1 arg 
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv) - 1
+    if args < 1:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+    else:
+        print("{} argments:".format(args))
+    for i in range(args):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
