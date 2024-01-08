9962d8dc149dfc2c318f4f2b78043f8a84a6b9a9
Mwan3kii <agathawanja303@gmail.com>
2024-01-08 18:52:38 +0300

    adds tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a < 2:
-        i = 0
-    elif tuple_a > 2:
-        bkjs
-    else:
-        tuple_a + tuple_b
+    new_tuple = ()
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return new_tuple
