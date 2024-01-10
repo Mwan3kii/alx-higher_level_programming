bcc763e4544b413539d1ed6a1d864a360a045021
Mwan3kii <agathawanja303@gmail.com>
2024-01-10 22:46:02 +0300

    squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     squared_list = []
     for row in matrix:
-            squared_list.append([elem ** 2 for elem in row])
+        squared_list.append([elem**2 for elem in row])
     return squared_list
