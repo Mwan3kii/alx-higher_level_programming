592349055fdd5d3949cec10f418e0d6f19d10797
Mwan3kii <agathawanja303@gmail.com>
2024-01-10 23:02:50 +0300

    replace/adds values in key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-
+def print_sorted_dictionary(a_dictionary):
+    sort_keys = sorted(a_dictionary.keys())
+    for key in sort_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
