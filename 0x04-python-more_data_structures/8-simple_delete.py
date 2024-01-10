f38aa7018a131f7d1d290c3148fc3c4801e9d072
Mwan3kii <agathawanja303@gmail.com>
2024-01-10 23:14:27 +0300

    deletes key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    sort_keys = sorted(a_dictionary.keys())
+    for key in sort_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
