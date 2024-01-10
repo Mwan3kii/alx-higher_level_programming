378dce3b0da12f58d2fc8336eb4966294b63788a
Mwan3kii <agathawanja303@gmail.com>
2024-01-10 23:06:01 +0300

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,8 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
+
 def print_sorted_dictionary(a_dictionary):
-    sort_keys = sorted(a_dictionary.keys())
-    for key in sort_keys:
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
         print("{}: {}".format(key, a_dictionary[key]))
