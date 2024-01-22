0ba84c9d6362b6c36b28253ad34c05f3508cfceb
Mwan3kii <agathawanja303@gmail.com>
2024-01-22 20:25:11 +0300

    error msg

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as e:
+        error_message = f"Exception: {e}"
+        print(error_message, file=sys.stderr)
+        return False
