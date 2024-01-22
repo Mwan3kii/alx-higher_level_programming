558c09ea915fa76aae8928fb100ed9d67e1e629b
Mwan3kii <agathawanja303@gmail.com>
2024-01-22 22:00:55 +0300

    prints interger with erro

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        error_message = f"Exception: {e}"
-        print(error_message, file=sys.stderr)
+    except Exception as e:
+        print("Expection: {}".format(e), file=sys.stderr)
         return False
