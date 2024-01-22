75e43f9ef1f49a708d88d669afe1c139d95c4ad8
Mwan3kii <agathawanja303@gmail.com>
2024-01-22 17:55:06 +0300

    prints only ints

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for elem in range(x):
+            value = my_list[elem]
+            if isinstance(value, int):
+                print("{:d}".format(value), end="")
+                count += 1
+    except IndexError:
+        pass
+    print()
+    return count
