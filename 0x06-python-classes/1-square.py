c1998bb1c868154b005a934e174d9a6fea7703de
Mwan3kii <agathawanja303@gmail.com>
2024-01-24 16:43:47 +0300

    add private attr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is the square class.
+
+It has one private attribute.
+"""
+class Square:
+    """
+    This is the Square class.
+
+    Attributes:
+        One private attribute so far
+    """
+    def __init__(self, size):
+        """the init method.
+
+        Args:
+            size: the size of the square
+
+        Returns:
+            size: the size of the square
+        """
+        self.__size = size
+        pass
