95bcec415f076b2f9d1889c31ccbe4a7ccb0e070
Mwan3kii <agathawanja303@gmail.com>
2024-01-30 19:28:21 +0300

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""This is class that prevents objects or attributes."""
+
+
 class LockedClass:
+    """This is a Locked class that prevents user
+    from creating new instances attributes.
+    """
     __slots__ = ("first_name")
