32d669c2d12a76414f4abbde0c8c2bf4adcfdf2e
Mwan3kii <agathawanja303@gmail.com>
2024-02-02 13:20:49 +0300

    prints string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+"""This prints a string."""
+
+
 def say_my_name(first_name, last_name=""):
+    """This prints a string.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name.
+
+    Raises:
+        TypeError: Fist name and last name must be string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
