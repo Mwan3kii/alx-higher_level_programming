ff28f3c4d1f7884287f9c2f6d250c22f291c9f69
Mwan3kii <agathawanja303@gmail.com>
2024-02-05 22:45:45 +0300

    spaces

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,7 @@
 
 class MyList(list):
     """This class inherits from list."""
-
     def print_sorted(self):
-        """This prints list in sorted order.
-        """
+        """This prints list in sorted order."""
         new_list = sorted(self)
         print(new_list)
