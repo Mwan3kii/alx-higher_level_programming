52ca4e0a48c92580910ccc0a180106120410954e
Mwan3kii <agathawanja303@gmail.com>
2024-02-05 21:40:12 +0300

    raise eception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines area in class."""
+
+
+class BaseGeometry():
+    """Defines an area but not implemented."""
+
+    def area(self):
+        """Defines an area in class.
+
+        Raises:
+            Exception: area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
