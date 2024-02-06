adfadb54dc5b60e93e2f88c5b2469106229ef3f5
Mwan3kii <agathawanja303@gmail.com>
2024-02-06 16:32:06 +0300

    writes string

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
+"""This writes a string with with."""
 
+
+def write_file(filename="", text=""):
+    """This writes a string and returns number of character.
+
+    Args:
+        filename: The filename.
+        text: The text to use.
+
+    Returns:
+        str: The number of character.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        return file.write(text)
