9d0bd8a193c82f1a98780c62d3cb9e688ff28184
Mwan3kii <agathawanja303@gmail.com>
2024-02-06 22:59:04 +0300

    changes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def add_items_to_list(arguments):
-    arguments = list(sys.argv[1:])
-    try:
-        existing_list = load_from_json_file("add_item.json")
-    except Exception:
-        existing_list = []
-    existing_list.extend(arguments)
-    save_to_json_file(existing_list, "add_item.json")
+arguments = list(sys.argv[1:])
+try:
+    existing_list = load_from_json_file('add_item.json')
+except Exception:
+    existing_list = []
+existing_list.extend(arguments)
+save_to_json_file(existing_list, 'add_item.json')
