606fd6c144e91962d9cb72706327796b0e9abc1a
Mwan3kii <agathawanja303@gmail.com>
2024-02-07 19:51:16 +0300

    pycodestyle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,21 +4,21 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-        '''A subclass representing a rectangle.'''
-        def __init__(self, size):
-            """Defines a size.
+    """This does area of rectangele."""
 
-            Args:
-                size: The size.
-            """
-            self.integer_validator("size", size)
-            self.__size = size
-            super().__init__(size, size)
+    def __init__(self, size):
+        """Does area of square.
+        Args:
+            size: The size.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-        def area(self):
-            """Does area.
+    def area(self):
+        """Does area.
 
-            Returns:
-                area: The area.
-            """
-            return self.__size ** 2
+        Returns:
+            area: The area.
+        """
+        return self.__size ** 2
