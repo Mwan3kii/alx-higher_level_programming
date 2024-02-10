dd03998110bfdfde93eade195da49d6dbbd6c18f
Mwan3kii <agathawanja303@gmail.com>
2024-02-10 20:50:29 +0300

    rectangle1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""This defines class Rectangle with subclass Base."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes private instance attributes.
+
+        Args:
+            id (int): Unique identifier for each instance
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int): X-coordinate of the rectangle.
+            y (int): Y-coordinate of the rectangle.
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        self.__width = width
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        if not isinstance(value, int) and value > 0:
+            raise ValueError("width must be integer")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        if not isinstance(value, int) and value > 0:
+            raise ValueError("height must be integer")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x."""
+        if not isinstance(value, int):
+            raise ValueError("x must be integer")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y."""
+        if not isinstance(value, int):
+            raise ValueError("y must be integer")
+        else:
+            self.__y = value
