a8ce39017f7d3982a9c30b42d66d5e7ad9ef045d
Mwan3kii <agathawanja303@gmail.com>
2024-02-11 16:44:15 +0300

    sets attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,9 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """Sets arguments to attributes."""
+        attributes = ['id', 'width', 'height', 'x', 'y']
+        for i in range(len(args)):
+            setattr(self, attributes[i], args[i])
