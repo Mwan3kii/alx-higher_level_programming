3ae0d0ddb903bdf2b3aa9d9340ba6b2605e32e09
Mwan3kii <agathawanja303@gmail.com>
2024-02-11 18:21:36 +0300

    gets sets size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,19 @@ class Square(Rectangle):
         return "[Square] ({}) {:d}/{:d} - {:d}".format(
                 self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """Gets the size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
