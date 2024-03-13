a03e29c52f28ea1af6b4d872560334950a5cc152
Mwan3kii <agathawanja303@gmail.com>
2024-03-14 00:52:52 +0300

    update

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT CONCAT('first_table', '     ', CREATE_TABLE)
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
-
+SHOW CREATE TABLE hbtn_0c_0.first_table;
