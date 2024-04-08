22daede5429b019fb35479d7b2eab4ea2c1c6a54
Mwan3kii <agathawanja303@gmail.com>
2024-04-08 22:07:09 +0300

    obje facr

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 // adding a new function incr that increments the integer value
-function incr () {
-  this.value++;
-}
 const myObject = {
-	  type: 'object',
-	  value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject.incr = incr;
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
