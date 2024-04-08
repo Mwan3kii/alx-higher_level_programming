dca33dee958789f569cf1021840950e9f380d7e6
Mwan3kii <agathawanja303@gmail.com>
2024-04-08 21:01:56 +0300

    square

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 // prints x times C is fun
 const arg = process.argv[2];
 const x = parseInt(arg);
-if (!isNaN(x) && x > 0) {
+if (!isNaN(x) || x > 0) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
