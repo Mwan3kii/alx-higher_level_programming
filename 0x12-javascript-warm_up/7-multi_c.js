a36c6c085a987fbfb72cf32d47371d43bca0c7b6
Mwan3kii <agathawanja303@gmail.com>
2024-04-08 20:52:34 +0300

    prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// prints x times C is fun
+const arg = process.argv[2];
+const x = parseInt(arg);
+if (!isNaN(x) && x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
