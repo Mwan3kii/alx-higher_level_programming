8fc33f17297699daea5b90213c68ca14bf658368
Mwan3kii <agathawanja303@gmail.com>
2024-04-08 20:46:04 +0300

    for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints 3 lines using array of string and loop
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
