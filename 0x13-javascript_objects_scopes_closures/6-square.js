0af1f38cd2ea8b87fdc038127f9609d96156b982
Mwan3kii <agathawanja303@gmail.com>
2024-04-09 21:07:13 +0300

    square x and c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,8 +5,8 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 };
