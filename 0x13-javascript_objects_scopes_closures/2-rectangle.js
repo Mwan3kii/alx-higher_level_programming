921d8c7d6033a0eed047633aa3a69dc1697e527a
Mwan3kii <agathawanja303@gmail.com>
2024-04-09 19:37:32 +0300

    is valid

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,12 @@
 // empty class Rectangle that defines a rectangle
 class Rectangle {
   constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    }
     this.width = w;
     this.height = h;
   }
 }
+
 module.exports = Rectangle;
