66bdb277333af6fa24b91b05c60aaa54e5f5bd1c
Mwan3kii <agathawanja303@gmail.com>
2024-04-09 20:33:14 +0300

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   let start = 0;
   let end = list.length - 1;
   while (start < end) {
-    let tmp = list[start];
+    const tmp = list[start];
     list[start] = list[end];
     list[end] = tmp;
     start++;
