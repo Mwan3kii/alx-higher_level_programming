c20649e6a4f906a6c18227887bb430919db0a40b
Mwan3kii <agathawanja303@gmail.com>
2024-04-25 21:20:28 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,15 @@
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
-
-    def help_peak(low, high):
-        if high - low == 1:
-            return list_of_integers[low]
+    low = 0
+    high = len(list_of_integers) - 1
+    while low <= high:
         mid = low + (high - low) // 2
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == high - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and\
+           (mid == high or list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            return help_peak(mid + 1, high)
-        return help_peak(low, mid)
-    return help_peak(0, len(list_of_integers))
+        if mid < high and list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
