e47904e4d23a3b35851665ff04afd2eabb4265d7
Mwan3kii <agathawanja303@gmail.com>
2024-04-25 21:05:47 +0300

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script sends a GET request to URL and displays the body of the response
-curl -s -w "\n%{http_code}" "$1" | awk 'NR==1 { body=$0; } NR==2 && $0 == "200" { print body; }'
+curl -sL "$1"
