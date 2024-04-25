1df1f57b2a59f3c894b3a0c3481c4cf54e281caa
Mwan3kii <agathawanja303@gmail.com>
2024-04-25 20:56:51 +0300

    100-status_code.sh

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,13 @@ def find_peak(list_of_integers):
         return None
 
     def help_peak(low, high):
-        mid = (low + high) // 2
-        if (
-            (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])
-            and (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1])
-        ):
+        if high - low == 1:
+            return list_of_integers[low]
+        mid = low + (high - low) // 2
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == high - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
-        if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
-            return help_peak(low, mid - 1)
-        return help_peak(mid + 1, high)
-    return help_peak(0, len(list_of_integers) - 1)
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            return help_peak(mid + 1, high)
+        return help_peak(low, mid)
+    return help_peak(0, len(list_of_integers))
