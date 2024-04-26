f5871a991c22c12d52bffe4f8d587905f0f972ee
Mwan3kii <agathawanja303@gmail.com>
2024-04-26 20:18:28 +0300

    hbtn header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ with urllib.request.urlopen(url) as response:
     content = response.read()
     print("Body response:")
     print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}") 
+    print(f"\t- content: {content}")
     utf8_content = content.decode("utf-8", errors="replace")
     print(f"\t- utf8 content: {utf8_content}")
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    headers = response.headers
+    x_request_var = headers.get("X-Request-Id")
+    print(x_request_var)
